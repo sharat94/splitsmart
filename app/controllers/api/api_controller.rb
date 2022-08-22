c1c1c6a78893e024679a3b98ae92e07bb64cb710
hushimatata <sharat.oommen@finly.io>
2022-08-23 00:51:41 +0530

    remove unnecessary rescues

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -14,11 +14,7 @@ class Api::ApiController < ActionController::API
     render_error(e.message, :unprocessable_entity)
   rescue ActiveRecord::RecordNotFound
     render_error("Resource not found", :not_found)
-  rescue UnauthorizedError => e
-    render_error(e.message, :unauthorized)
-  rescue ActionController::UnpermittedParameters => e
-    render_error("You are not allowed to perform this action, #{e.message}", :forbidden)
-  end
+   end
 
   def render_error(error_message, status)
     render json: { message: error_message }, status: status
